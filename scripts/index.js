500c4a9884f45048e096cacc09f9167eac8add6c
BakayAlexander <bakay.dvr@gmail.com>
2021-10-18 17:58:54 +0300

    fixed: after first review

--- scripts/index.js
+++ scripts/index.js
@@ -8,12 +8,28 @@ const popupCloseButtonElement = popupElement.querySelector('.popup__button-close
 //Выборка кнопки открытия 
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 
+//------------------------------------
+
+//ФОРМА. Объявление переменных и выборки
+//Выборка формы
+let formElement = popupElement.querySelector('.popup__container');
+
+//Выборка текстовых элементов
+let nameElement = document.querySelector('.profile__name');
+let descriptionElement = document.querySelector('.profile__description');
+
+//Выборка элементов формы input
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let descriptionInput = formElement.querySelector('.popup__input_type_description');
 
 //Функции
 
 //Функция открытия popup
 const openPopup = function() {
     popupElement.classList.add('popup_is-opened');
+    //Записываем текущие данные с сайта в input
+    nameInput.value = nameElement.textContent;
+    descriptionInput.value = descriptionElement.textContent;
 };
 
 //Функция закрытия popup
@@ -29,40 +45,7 @@ const closePopupByClickOverlay = function(event) {
     closePopup();
 };
 
-
-//События 
-//Событие 'Открытие popup' 
-popupOpenButtonElement.addEventListener('click', openPopup)
-
-//Событие 'Закрытие popup'
-popupCloseButtonElement.addEventListener('click', closePopup);
-
-//Событие 'Закрытие popup при клике на пустую область'
-popupElement.addEventListener('click', closePopupByClickOverlay);
-
-
-
-
-
-//Реализация отправки формы
-
-//Объявление переменных и выборки
-//Выборка формы
-let formElement = popupElement.querySelector('.popup__container');
-
-//Выборка текстовых элементов
-let nameElement = document.querySelector('.profile__name');
-let descriptionElement = document.querySelector('.profile__description');
-
-//Выборка элементов формы input
-let nameInput = formElement.querySelector('.popup__name');
-let descriptionInput = formElement.querySelector('.popup__description');
-
-//Подтягиваем текущие значения текстовых элементов в input
-nameInput.value = nameElement.textContent;
-descriptionInput.value = descriptionElement.textContent;
-
-//Функция 
+//Функция заполнения формы по кнопке "Сохранить"
 function formSubmitHandler(evt) {
     evt.preventDefault();
     //Вытаскиваем текущие значения input
@@ -74,7 +57,15 @@ function formSubmitHandler(evt) {
     closePopup();
 }
 
+//События 
+//Событие 'Открытие popup' 
+popupOpenButtonElement.addEventListener('click', openPopup)
+
+//Событие 'Закрытие popup'
+popupCloseButtonElement.addEventListener('click', closePopup);
+
+//Событие 'Закрытие popup при клике на пустую область'
+popupElement.addEventListener('click', closePopupByClickOverlay);
+
+//Событие 'Клик по кнопке Сохранить'
 formElement.addEventListener('submit', formSubmitHandler); 
-
-
-
